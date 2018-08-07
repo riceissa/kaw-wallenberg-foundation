5cb308250916f8a475b2abd60bef8fde75a974c8
Issa Rice <riceissa@gmail.com>
2018-08-07 11:36:08 -0700

    do more

--- proc_research_projects.py
+++ proc_research_projects.py
@@ -19,12 +19,13 @@ def soup_to_grants(soup):
     for item in soup.find_all("div", {"class": "list-item__content"}):
         project_title = item.find("h2").text
         amount_investigator = item.find("p", {"class": "list-item__post-title"}).text
+        amount_investigator = amount_investigator.replace("\u2028", "")  # Remove line separator character
         assert ("Grant:" in amount_investigator and "Principal Investigator:" in amount_investigator) or "Final grant:" in amount_investigator or ("Beviljat anslag" in amount_investigator and "Principal Investigator:" in amount_investigator) or "Grant:" in amount_investigator
 
         if "Grant:" in amount_investigator and ("Principal Investigator:" in amount_investigator or "Principal investigator:" in amount_investigator):
             parts = amount_investigator.split("\n")
             assert len(parts) == 2
-            if parts[0].startswith("Grant:") or parts[0].startswith("\u2028Grant:"):
+            if parts[0].startswith("Grant:"):
                 assert parts[1].startswith("Principal Investigator:") or parts[1].startswith("Principal investigator:")
                 amount_part = parts[0].strip()
                 investigator_part = parts[1].strip()
@@ -34,6 +35,19 @@ def soup_to_grants(soup):
                 amount_part = parts[1].strip()
                 investigator_part = parts[0].strip()
             print(amount_part, investigator_part)
+        elif "Beviljat anslag:" in amount_investigator and "Principal Investigator:" in amount_investigator:
+            parts = amount_investigator.split("\n")
+            assert parts[0].startswith("Beviljat anslag:")
+            assert parts[1].startswith("Principal Investigator:")
+            amount_part = parts[0]
+            investigator_part = parts[1]
+        elif "Final grant:" in amount_investigator:
+            amount_part = amount_investigator.strip()
+            investigator_part = ""
+        elif "Grant:" in amount_investigator:
+            pass
+        else:
+            raise ValueError("We don't know this project format.")
 
         # print(find_focus_area(item), project_title)
 
