a5ce0194b8407b593ec63185a02199d5cf277588
Issa Rice <riceissa@gmail.com>
2018-08-07 11:23:20 -0700

    do one case

--- proc_research_projects.py
+++ proc_research_projects.py
@@ -21,12 +21,27 @@ def soup_to_grants(soup):
         amount_investigator = item.find("p", {"class": "list-item__post-title"}).text
         assert ("Grant:" in amount_investigator and "Principal Investigator:" in amount_investigator) or "Final grant:" in amount_investigator or ("Beviljat anslag" in amount_investigator and "Principal Investigator:" in amount_investigator) or "Grant:" in amount_investigator
 
-        print(find_title(item), project_title)
+        if "Grant:" in amount_investigator and ("Principal Investigator:" in amount_investigator or "Principal investigator:" in amount_investigator):
+            parts = amount_investigator.split("\n")
+            assert len(parts) == 2
+            if parts[0].startswith("Grant:") or parts[0].startswith("\u2028Grant:"):
+                assert parts[1].startswith("Principal Investigator:") or parts[1].startswith("Principal investigator:")
+                amount_part = parts[0].strip()
+                investigator_part = parts[1].strip()
+            else:
+                assert parts[1].startswith("Grant:")
+                assert parts[0].startswith("Principal Investigator:") or parts[0].startswith("Principal investigator:")
+                amount_part = parts[1].strip()
+                investigator_part = parts[0].strip()
+            print(amount_part, investigator_part)
+
+        # print(find_focus_area(item), project_title)
 
 
-def find_title(tag):
+def find_focus_area(tag):
     """Go up and find the most recent h2."""
-    # pdb.set_trace()
+    # This depends on the specific div nesting used on the website, and might
+    # break in the future
     tag = tag.parent.parent.parent
     while tag is not None and tag.name != "h2":
         tag = tag.previous_sibling
