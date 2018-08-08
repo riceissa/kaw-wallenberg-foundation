3b3a1fb87311fa3689b673cb41267d91ad26afdc
Issa Rice <riceissa@gmail.com>
2018-08-08 15:42:38 -0700

    change to start/end year

--- proc_manual_data.py
+++ proc_manual_data.py
@@ -8,18 +8,18 @@ def main():
     with open("manual_data.tsv", "r") as f:
         next(f)  # skip header row
         for line in f:
-            (grant_type, url, start_date, end_date, donee, donation_earmark,
+            (grant_type, url, start_year, end_year, donee, donation_earmark,
              sek_amount, project, focus_area) = line[:-1].split("\t")
             sek_amount = float(sek_amount)
-            if start_date:
-                usd_amount = sek_to_usd(sek_amount, int(start_date[:len("YYYY")]))
+            if start_year:
+                usd_amount = sek_to_usd(sek_amount, int(start_year))
             else:
                 usd_amount = sek_to_usd(sek_amount, 2016)  # TODO change?
             notes = []
             if project:
                 notes.append("project: " + project)
-            if start_date and end_date:
-                notes.append(f"grant period: {start_date} to {end_date}")
+            if start_year and end_year:
+                notes.append(f"grant period: {start_year} to {end_year}")
             notes_str = "; ".join(notes)
             if notes_str:
                 notes_str = notes_str[0].upper() + notes_str[1:]
@@ -28,8 +28,8 @@ def main():
                 mysql_quote(donee),  # donee
                 mysql_quote(donation_earmark),  # donation_earmark
                 str(usd_amount),  # amount
-                mysql_quote(start_date if start_date else ""),  # donation_date
-                mysql_quote("year" if start_date else ""),  # donation_date_precision
+                mysql_quote(start_year + "-01-01" if start_year else ""),  # donation_date
+                mysql_quote("year" if start_year else ""),  # donation_date_precision
                 mysql_quote("donation log"),  # donation_date_basis
                 mysql_quote(focus_area),  # cause_area
                 mysql_quote(url),  # url
@@ -37,7 +37,7 @@ def main():
                 mysql_quote(notes_str),  # notes
                 str(sek_amount),  # amount_original_currency
                 mysql_quote("SEK"),  # original_currency
-                mysql_quote(start_date if start_date else "2016-01-01"),  # currency_conversion_date, TODO change 2016?
+                mysql_quote(start_year + "-01-01" if start_year else "2016-01-01"),  # currency_conversion_date, TODO change 2016?
                 mysql_quote("Fixer.io"),  # currency_conversion_basis
             ]) + ")")
             first = False
